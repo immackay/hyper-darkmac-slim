b3c557f6d51c770a1ecab34ce1c7170ebb687b07
Ian Mackay <ianmackay00@gmail.com>
2018-10-26 05:42:41 -0700

    First commit

---
+++ index.js
@@ -0,0 +1,50 @@
+// Native
+const fs = require('fs')
+const path = require('path')
+
+const styles = fs.readFileSync(path.join(__dirname, 'styles.css'), 'utf8')
+
+const colors = {
+  black: '#000000',
+  red: '#990000',
+  green: '#00A600',
+  yellow: '#999900',
+  blue: '#0000B2',
+  magenta: '#B200B2',
+  cyan: '#00A6B2',
+  white: '#BFBFBF',
+  lightBlack: '#666666',
+  lightRed: '#E50000',
+  lightGreen: '#00D900',
+  lightYellow: '#E5E500',
+  lightBlue: '#0000FF',
+  lightMagenta: '#E500E5',
+  lightCyan: '#00E5E5',
+  lightWhite: '#E5E5E5'
+}
+
+exports.decorateConfig = config => Object.assign({}, config, {
+  padding: '25px 7px 7px 7px',
+  fontFamily: '"Fira Code", Menlo, "DejaVu Sans Mono", Consolas, "Lucida Console", monospace',
+  backgroundColor: '#1e1e1e',
+  foregroundColor: '#fff',
+  css: (config.css || '') + styles,
+  colors
+})
+
+exports.decorateBrowserOptions = defaults => Object.assign({}, defaults, {
+  titleBarStyle: 'hidden',
+  transparent: true
+})
+
+exports.getTabsProps = (parentProps, props) => {
+  const bodyClasses = document.body.classList
+
+  if (props.tabs.length <= 1) {
+    bodyClasses.add('closed-tabs')
+  } else {
+    bodyClasses.remove('closed-tabs')
+  }
+
+  return Object.assign({}, parentProps, props)
+}
